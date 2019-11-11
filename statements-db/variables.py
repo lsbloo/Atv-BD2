6eca7d584c229f039a358d5a5d63983557ae8f70
lsbloo <osvaldo_cavalcanti_filho@hotmail.com>
2019-11-10 23:03:24 -0300

    ok

---
+++ statements-db/variables.py
@@ -0,0 +1,11 @@
+import os
+
+
+
+
+DATABASE_NAME = os.environ.get('DATABASE_NAME','Not Set Name Database')
+DATABASE_HOST_CONNECT= os.environ.get('DATABASE_HOST','Not Set Host Connect')
+DATABASE_USER = os.environ.get('DATABASE_USER', 'Not Set  User ')
+DATABASE_PASSWORD = os.environ.get('DATABASE_PASSWORD', 'Not Set Password Database')
+DATABASE_PORT = os.environ.get('DATABASE_PORT', 'Not Set Port Database')
+
